afc49a1a4dba885e42b6210268a77a5e471d6e94
memorylorry <294092869@qq.com>
2019-05-10 17:04:32 +0800

    改用commons-lang的StringUtils

--- src/main/java/com/github/memorylorry/type/helper/FilterOptionHelper.java
+++ src/main/java/com/github/memorylorry/type/helper/FilterOptionHelper.java
@@ -3,7 +3,7 @@ package com.github.memorylorry.type.helper;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.github.memorylorry.type.Filter;
-import com.sun.deploy.util.StringUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
